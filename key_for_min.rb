91a41fb78d1846797bd4ce2a514c297118952f7a
Rodney Smith II <r_smith3@hotmail.com>
2020-09-02 00:50:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,9 @@
 def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     min_value = 1000
+    min_value = value
     if value <= min_value
-      key
+      
     end
     return key
  end
